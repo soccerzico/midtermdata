232f30eda27d6b0788fc88c2dc67dfb9f7fae203
Zaki Adwan <soccerzico@gmail.com>
2024-10-25 17:22:00 -0500

    Update FileProcessingScript.sh

--- FileProcessingScript.sh
+++ FileProcessingScript.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-message="hi"
+
+#definition of base directory and subdirectory names as an array
+dataSubDirectories=("takeoff" "land" "left" "right" "forward" "backward")
+baseDirectory="/home/zakiadwan2003/midtermdata/data"
+#replace path with desired path
+
+for subDirectory in "${dataSubDirectories[@]}"; do
+directoryPath="${baseDirectory}/${subDirectory}"
+[[ -d "$directoryPath" ]] || continue
+
+echo "Processing category directory: $directoryPath"
+
+
+
