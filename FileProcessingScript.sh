951fedc290c415041cd8a838d85c396a29baeea2
Zaki Adwan <soccerzico@gmail.com>
2024-10-25 23:54:22 -0500

    Final Update FileProcessingscript.h
    Completed functionality of first set of loops, and added second set of loops to clean up and rename. Also cleaned up formatting

--- FileProcessingScript.sh
+++ FileProcessingScript.sh
@@ -2,7 +2,7 @@
 
 # Definition of base directory and subdirectory names as an array
 dataSubDirectories=("takeoff" "land" "left" "right" "forward" "backward")
-baseDirectory="/home/zakiadwan2003/midtermdata/data2"
+baseDirectory="/home/zakiadwan2003/midtermdata/data8"
 
 # Loop through subdirectories
 for subDirectory in "${dataSubDirectories[@]}"; do
@@ -11,17 +11,32 @@ for subDirectory in "${dataSubDirectories[@]}"; do
 
     echo "Processing category directory: $directoryPath"
 
-    # Loop through each subdirectory of the category
+    # Loop through each sub-subdirectory 
     for subdir in "$directoryPath"/*; do
         [[ -d "$subdir" ]] || continue
         echo "Processing subdirectory: $subdir"
-        counter=1
-        # Loop through each sub-subdirectory and rename the files 1-n after shuffling them
+
+        # Shuffle files, then move all files to parent directory
         for file in $(find "$subdir" -type f | shuf); do
-        newFileName="$subdir/$counter.txt"
-        mv "$file" "$newFileName"
-        ((counter++))
+            mv "$file" "$directoryPath"
         done
+    done
+done
+
+# Second set of loops to iterate through and add sequential names to each of the newly moved files in each subdirectory
+# Also remove the sub-subdirectories, which are now unneeded
+for subDirectory in "${dataSubDirectories[@]}"; do
+    counter=1
+    directoryPath2="${baseDirectory}/${subDirectory}"
+    
+    for item in "$directoryPath2"/*; do
+        if [[ -d "$item" ]]; then
+            rm -r "$item"
+        else
+            newFileName="$directoryPath2/$counter.txt"
+            mv "$item" "$newFileName"
+            ((counter++))
+        fi
     done
 done
 
